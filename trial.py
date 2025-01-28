34d83b9d91078c07df54684f408168223b1a3e7d
Radithiyansusil <adithiyan2001@gmail.com>
2025-01-29 01:37:51 +0530

    Removed Unwanted

--- trial.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyDGcXW3GY6_IGBXAEjieryd0OMkZwrsm58")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
